13b82e63809fd7e8746184402543737bddb76d66
che2rish <88181171+che2rish@users.noreply.github.com>
2023-03-07 10:18:26 +0900

    입양 시각 구하기(1)

---
+++ programmers/Lv2/입양시각구하기(1).sql
@@ -0,0 +1,16 @@
+/* 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다. 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 
+   이때 결과는 시간대 순으로 정렬해야 합니다.*/
+
+-- mysql
+SELECT DATE_FORMAT(DATETIME,'%H') AS HOUR, COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR
+HAVING HOUR > 8 AND HOUR < 20
+ORDER BY HOUR;
+
+-- Oracle
+SELECT TO_NUMBER(TO_CHAR(DATETIME,'HH24')) AS HOUR , COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY TO_CHAR(DATETIME,'HH24')
+HAVING TO_CHAR(DATETIME,'HH24') > 8 AND TO_CHAR(DATETIME,'HH24') < 20
+ORDER BY HOUR;
