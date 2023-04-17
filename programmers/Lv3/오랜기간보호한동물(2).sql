925627b905650cbed2056da05b5ae1e466e2c5bc
che2rish <88181171+che2rish@users.noreply.github.com>
2023-04-17 13:16:03 +0900

    오랜 기간 보호한 동물(2)

---
+++ programmers/Lv3/오랜기간보호한동물(2).sql
@@ -0,0 +1,20 @@
+/* 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 
+   이때 결과는 보호 기간이 긴 순으로 조회해야 합니다. */
+
+-- mysql
+SELECT X.ANIMAL_ID, X.NAME
+FROM ( SELECT A.*
+        FROM ANIMAL_INS A, ANIMAL_OUTS B
+        WHERE A.ANIMAL_ID = B.ANIMAL_ID
+        ORDER BY B.DATETIME - A.DATETIME DESC
+)X
+LIMIT 2;
+
+-- Oracle
+SELECT X.ANIMAL_ID, X.NAME
+FROM ( SELECT A.*
+        FROM ANIMAL_INS A, ANIMAL_OUTS B
+        WHERE A.ANIMAL_ID = B.ANIMAL_ID
+        ORDER BY B.DATETIME - A.DATETIME DESC
+)X
+WHERE ROWNUM < 3;
